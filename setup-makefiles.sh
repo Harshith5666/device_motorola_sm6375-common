2f6cb343934f2676819fc2dea4f896f058e9fc8f
Harshith5666 <bgmi18461846@gmail.com>
2025-05-07 09:57:36 +0000

    Revert "sm6375-common: Update extraction scripts from latest available template"
    This reverts commit 70cabb51680ad2e3a01c39cb774e290ca4429342.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
-# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
-# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
+
+DEVICE=sm6375-common
+VENDOR=motorola
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -13,57 +17,20 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils-old/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-function vendor_imports() {
-    cat <<EOF >>"$1"
- 		"device/motorola/sm6375-common",
-         "hardware/motorola",
-         "hardware/qcom-caf/wlan",
-         "hardware/qcom-caf/sm8350",
-         "vendor/qcom/opensource/usb/etc",
-         "vendor/qcom/opensource/commonsys-intf/display",
-         "vendor/qcom/opensource/commonsys/display",
-         "vendor/qcom/opensource/display",
-         "vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
-         "vendor/qcom/opensource/dataservices",
-EOF
-}
-
-function lib_to_package_fixup() {
-    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
-        lib_to_package_fixup_proto_3_9_1 "$1" ||
-        lib_to_package_fixup_vendor_variants "$@"
-}
-
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Warning headers and guards
-write_headers "bangkk corfur fogos penang miami rhodep"
+write_headers
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
-
-if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    source "${MY_DIR}/../../${VENDOR}/${DEVICE}/setup-makefiles.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
-
-    # Warning headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt"
-
-    # Finish
-    write_footers
-fi
