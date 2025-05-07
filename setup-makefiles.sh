c8f036288bc1c368744c0281b805b71cbc8feaba
Harshith5666 <bgmi18461846@gmail.com>
2025-05-07 09:51:44 +0000

    Revert "sm6375-common: Disable ELF checks on device specific libs"
    This reverts commit 8c541beddbae1d6b1003a2e34f15e300c03a7874.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -56,7 +56,6 @@ function lib_to_package_fixup_vendor_variants() {
             libthermalclient | \
             libqsap_sdk | \
             libril | \
-            libthermalclient | \
             libwpa_client | \
             vendor.qti.qspmhal@1.0) ;;
         *)
