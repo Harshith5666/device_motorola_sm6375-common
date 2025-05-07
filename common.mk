88614f33c8414ea7bb5ac312c61df146cde2a5fc
Harshith5666 <bgmi18461846@gmail.com>
2025-05-07 09:47:53 +0000

    Revert "sm6375-common: Remove sec_config from tree"
    This reverts commit 89f114463f485101412fd01b6b64dcfd84cf37fe.

--- common.mk
+++ common.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
