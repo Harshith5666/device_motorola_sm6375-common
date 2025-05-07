2d74954ccfeb8fb761ed7fa0dc79330811481e91
Harshith5666 <bgmi18461846@gmail.com>
2025-05-07 09:50:32 +0000

    Revert "sm6375-common: Patch libwfdservice to resolve duplicate dependencies"
    This reverts commit 33a90961613db1d0ebe777a44234efd10f7c0013.

--- extract-files.sh
+++ extract-files.sh
@@ -70,10 +70,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
-        system_ext/lib/libwfdservice.so)
-            [ "$2" = "" ] && return 0
-            ${PATCHELF} --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
-            ;;
         system_ext/etc/permissions/moto-telephony.xml)
             [ "$2" = "" ] && return 0
             sed -i "s#/system/#/system_ext/#" "${2}"
